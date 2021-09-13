4789ff4c6df608050c6cb981213eac2f30a07c39
dangchengcheng <chengzi>
2021-09-13 16:16:57 +0800

    add switch branch shell

---
+++ bin/gitSwitchToGithub.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+cd existing_repo
+git remote rename github-origin
+git remote rename origin local-origin
+git remote add origin git@github.com:Space-Huntsman/KarmanUI.git
+git push -u origin --all
+git push -u origin --tags
