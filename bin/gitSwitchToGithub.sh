c4c44f587041bf3911bb3c15a0ed9c53d1c7ba06
dangchengcheng <chengzi>
2021-09-13 16:43:57 +0800

    update

--- bin/gitSwitchToGithub.sh
+++ bin/gitSwitchToGithub.sh
@@ -2,5 +2,7 @@
 git remote rename github-origin
 git remote rename origin local-origin
 git remote add origin git@github.com:Space-Huntsman/KarmanUI.git
-git push -u origin --all
-git push -u origin --tags
+git pull
+# 思考良久决定还是让手动向上推
+#git push -u origin --all
+#git push -u origin --tags
--- bin/gitSwitchToLocal.sh
+++ bin/gitSwitchToLocal.sh
@@ -2,5 +2,8 @@
 git remote remove local-origin
 git remote rename origin github-origin
 git remote add origin ssh://git@gitlab.hl.xa:9002/wangwei/karman-ui.git
-git push -u origin --all
-git push -u origin --tags
+git pull
+# 思考良久决定还是让手动向上推
+#git push -u origin --all
+#git push -u origin --tags
+
