7e62981b45fc66e7f5ae3c8892e0315188b78e38
nick <dc2002007@gmail.com>
2022-03-18 15:16:30 +0800

    fixed datepicker mouth to lang text show error

--- src/utils/date-util.js
+++ src/utils/date-util.js
@@ -14,10 +14,10 @@ const newArray = function(start, end) {
 
 export const getI18nSettings = () => {
   return {
-    dayNamesShort: weeks.map(week => t(`el.datepicker.weeks.${ week }`)),
-    dayNames: weeks.map(week => t(`el.datepicker.weeks.${ week }`)),
-    monthNamesShort: months.map(month => t(`el.datepicker.months.${ month }`)),
-    monthNames: months.map((month, index) => t(`el.datepicker.month${ index + 1 }`)),
+    dayNamesShort: weeks.map(week => t(`long.datepicker.weeks.${ week }`)),
+    dayNames: weeks.map(week => t(`long.datepicker.weeks.${ week }`)),
+    monthNamesShort: months.map(month => t(`long.datepicker.months.${ month }`)),
+    monthNames: months.map((month, index) => t(`long.datepicker.month${ index + 1 }`)),
     amPm: ['am', 'pm']
   };
 };
